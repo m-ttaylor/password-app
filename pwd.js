91992c258ce0c2103de9168615af8cac8ae88f6b
Matt <mttaylor@email.wm.edu>
2018-02-13 18:40:42 -0500

    added copy to clipboard button

--- pwd.js
+++ pwd.js
@@ -26,6 +26,11 @@ function generate_password() {
     else var pwd = words.join("");
 
     document.getElementById('password_result').innerText = pwd;
+
+    var panel = document.getElementById("copy_button");
+    if (panel.style.display !== "inline-block") {
+       panel.style.display = "inline-block";
+    }
 }
 
 function calculate_strength() {
@@ -36,46 +41,37 @@ function calculate_strength() {
     document.getElementById('entropy').innerText = entropy;
 }
 
-function show_options() {
-    var el = document.getElementById("advancedOptions");
-    if (el.style.display === "none") {
-        el.style.display = "block";
-    } else {
-        el.style.display = "none";
-    }
-}
-
 function pass_options() {
     spaces = document.getElementById("option1").checked;
     // put additional option effects here
 }
 
-function expand(el) {
-    if (el.value === "more options +") {
-        el.value = "more options -";
-    } else {
-        el.value = "more options +";
-    }
+function copyToClipboard() {
+    var textArea = document.createElement("textarea");
+    var pwd = document.getElementById('password_result').innerText
+    textArea.value = pwd;
+    document.body.appendChild(textArea);
+    textArea.select();
+    var success = document.execCommand("Copy");
+    console.log("copied to clipboard!");
+    document.body.removeChild(textArea);
 }
-
 //handle accordion menu
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
-// var el = document.getElementById("advancedOptions");
-// el.style.display = "none";
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+//
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function() {
+//         /* Toggle between adding and removing the "active" class,
+//         to highlight the button that controls the panel */
+//         this.classList.toggle("active");
+//
+//         /* Toggle between hiding and showing the active panel */
+//         var panel = this.nextElementSibling;
+//         if (panel.style.display === "block") {
+//             panel.style.display = "none";
+//         } else {
+//             panel.style.display = "block";
+//         }
+//     });
+// }
